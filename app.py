44c17db58427ab44df0ae523b27a060a9ddefca9
Girish Somashekar <srbindas70@gmail.com>
2021-04-06 19:59:51 -0400

    adding files april 6th

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
 from flask_pymongo import PyMongo
-
+from flask_cors import CORS, cross_origin
+from bson.json_util import dumps
+from bson.json_util import loads
 
 # Create an instance of Flask
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": "*"
+    }
+})
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_ORIGINS'] = '*'
+app.config['DEBUG'] = True
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb+srv://fan4_mongo:sGWQhhfqSBAcmFFn@cluster0.eqase.mongodb.net/movie_db")
@@ -25,20 +35,98 @@ def main():
     display_data = sorted_data["imdb_title_id","original_title","year","country","genre","avg_vote","votes"]
     box_office_data = sorted_data["imdb_title_id","original_title","budget","worldwide_gross_income"]
     return jsonify(display_data,box_office_data) 
-   
-@app.route("/top_movies/<countries>")
+
+@app.route("/countries")
 def countries():
 
     data = mongo.db.movies.find().distinct('country')
-    sorted_data = list(data.sort("avg_vote", -1)[0:10])
-    display_data = sorted_data["imdb_title_id","original_title","year","country","genre","avg_vote","votes"]
-    box_office_data = sorted_data["imdb_title_id","original_title","budget","worldwide_gross_income"]
+    country_list = list(data)
+    unique_country_list = []
+    sorted_country_dict = {}
+    # Iterate through the outer list
+    for countries in country_list:
+        c = countries.split(', ')
+        for c_list in c:
+            if c_list != "":
+                if c_list not in unique_country_list:
+                    unique_country_list.append(c_list)
+    unique_country_list.sort()
+    sorted_country_dict["country"] = unique_country_list
+    return jsonify(sorted_country_dict)  
 
-    return jsonify(display_data,box_office_data) 
+
+@app.route("/genres")
+def genres():
+
+    data = mongo.db.movies.find().distinct('genre')
+    genre_list = list(data)
+    unique_genre_list = []
+    sorted_genre_dict = {}
+    # Iterate through the outer list
+    for genres in genre_list:
+        g = genres.split(', ')
+        for genre_list in g:
+            if genre_list != "":
+                if genre_list not in unique_genre_list:
+                    unique_genre_list.append(genre_list)
+    unique_genre_list.sort()
+    sorted_genre_dict["genre"] = unique_genre_list
+    return jsonify(sorted_genre_dict)
+
+@app.route("/year")
+def year():
+
+    data = mongo.db.movies.find().distinct('year')
+    year_list = list(data)
+    sorted_year_dict = {}
+    sorted_year_dict["year"] = year_list
+    return jsonify(sorted_year_dict)             
+   
+@app.route("/top_movies")
+def countries_top10():
+    query_string = request.args
+    print(query_string)
+    mongo_query = {"votes": {"$gte":"300"}}
+    for k, v in query_string.items():
+        if k == "country":
+            mongo_query["country"] = v
+        elif k == "genre":
+            mongo_query["genre"] = v
+        elif k == "startyear":
+            mongo_query["year"] = {"$gte": v}
+        elif k == "endyear":
+            mongo_query["year"] = {"$lte": v}
+            
+    data = mongo.db.movies.find(mongo_query)
+    sorted_data = list(data.sort("avg_vote",-1)[0:10])
+    response = []
+    for document in sorted_data:
+        response.append({
+            "title": document["title"],
+            "country": document["country"],
+            "genre": document["genre"]
+        })
+        print(document["title"], document["country"], document["genre"])
+
+
+    # topmovies_bycountry = []
+    # topmovies = {}
+    # # display_data = sorted_data["imdb_title_id","original_title","year","avg_vote","votes"]
+    # # box_office_data = sorted_data["imdb_title_id","original_title","budget","worldwide_gross_income"]
+    # json_data = dumps(sorted_data)
+    # # for data in json_data:
+    # #     topmovies_bycountry.append(json_data[data])
+    # # for topmov in sorted_data:
+    # #     tm = topmov.split('_id')
+    # #     for tm_list in tm:
+    # #         topmovies_bycountry.append(tm_list)
+    # # topmovies["movies"] = topmovies_bycountry 
+    # topmovies["movies"] = json_data 
+    return jsonify(response)
     
 
-@app.route("/top_movies/<genre>")
-def genres():
+@app.route("/top_movies_by_genre/<genre>")
+def genres_top10(genre):
 
     data = mongo.db.movies.find().distinct('genre')
     sorted_data = list(data.sort("avg_vote", -1)[0:10])
@@ -47,8 +135,8 @@ def genres():
 
     return jsonify(display_data,box_office_data)      
 
-@app.route("/top_movies/<country>/<genre>")
-def top_movies(country, genre):
+@app.route("/top_movies_country_genre/<country>/<genre>")
+def top_movies_country_genre(country, genre):
 
     data = mongo.db.movies.find({"country": country,"genre":genre})
     sorted_data = list(data.sort("avg_vote", -1)[0:10])
@@ -57,8 +145,8 @@ def top_movies(country, genre):
 
     return jsonify(display_data,box_office_data) 
     
-@app.route("/top_movies/<startyear>/<endyear>")
-def top_movies(year):
+@app.route("/top_movies_year/<startyear>/<endyear>")
+def top_movies_year(startyear, endyear):
 
     startyear = startyear.astype('int64')
     endyear = endyear.astype('int64')
@@ -69,8 +157,8 @@ def top_movies(year):
 
     return jsonify(display_data,box_office_data)
 
-@app.route("/top_movies/<country>/<startyear>/<endyear>")
-def top_movies(country,year):
+@app.route("/top_movies_country_year/<country>/<startyear>/<endyear>")
+def top_movies_country_year(country,startyear,endyear):
 
     startyear = startyear.astype('int64')
     endyear = endyear.astype('int64')
@@ -82,7 +170,7 @@ def top_movies(country,year):
     return jsonify(display_data,box_office_data)
 
 @app.route("/top_movies/<genre>/<startyear>/<endyear>")
-def top_movies(genre,year):
+def top_movies_genre_year(genre,year):
 
     startyear = startyear.astype('int64')
     endyear = endyear.astype('int64')
--- data.js
+++ data.js
@@ -1,5 +1,117 @@
-d3.json("localhost:5000/top_movies/USA/Drama", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].title)
+// d3.json("http://127.0.0.1:5000/countries", function(data) {
+//     for (var i = 0; i < data.length; i++) {
+//         console.log(data[i].country)
+//     }
+// })
+
+function buildtable(query_string) {
+    console.log(query_string)
+    d3.json("http://127.0.0.1:5000/top_movies" + query_string).then((data)=> {
+        console.log(data)
+        
+        var tableData = data;
+
+
+        // Get a reference to the table body
+        var tbody = d3.select("tbody");
+        
+        tbody.html("");
+
+        data.forEach(item => {
+            var row = tbody.append("tr");
+            Object.entries(item).forEach(([key, value]) => {
+              var cell = row.append("td");
+              cell.text(value);
+            });
+        });
+        // Object.entries(data => {
+        //     console.log(data);
+        //     var row = tbody.append("tr");
+        //     Object.entries(data).forEach(([key, value]) => {
+        //       var cell = row.append("td");
+        //       cell.text(value);
+        //     });
+          
+        // });    
+        });
     }
-})
+
+function optionChanged(id) {
+        //Check the value of each options
+        //if value exists, build query string
+        // query_string="?"
+        // if country:
+            // query_string = query_string + "country=" + country + "&"
+        // if genre:
+            //query_string = query_string + "genre=" + genre + "&" 
+        buildtable(query_string);
+        // getDemoInfo(id);
+    } 
+
+function init() {
+         
+    var c_dropdown = d3.select("#selcountryDataset");
+    
+        
+    d3.json("http://127.0.0.1:5000/countries").then((c_data)=> {
+        console.log(c_data)
+    
+            
+        c_data.country.forEach(function(countries) {
+            c_dropdown.append("option").text(countries).property("value");
+            });
+
+        c_dropdown.property('value','USA');
+
+        buildtable("USA");      
+    
+        });
+        
+
+    var g_dropdown = d3.select("#selgenreDataset");
+    
+        
+    d3.json("http://127.0.0.1:5000/genres").then((g_data)=> {
+        console.log(g_data)
+        
+                
+        g_data.genre.forEach(function(genres) {
+            g_dropdown.append("option").text(genres).property("value");
+                });
+        
+        });
+
+    var startyear_dropdown = d3.select("#selstartyearDataset"); 
+    
+    d3.json("http://127.0.0.1:5000/year").then((startyear_data)=> {
+        console.log(startyear_data)
+        
+                
+        startyear_data.year.forEach(function(years) {
+            startyear_dropdown.append("option").text(years).property("value");
+                });
+
+                startyear_dropdown.property('value','2019');        
+        
+        });
+
+    var endyear_dropdown = d3.select("#selendyearDataset"); 
+    
+        d3.json("http://127.0.0.1:5000/year").then((endyear_data)=> {
+            console.log(endyear_data)
+            
+                    
+            endyear_data.year.forEach(function(years) {
+                endyear_dropdown.append("option").text(years).property("value");
+                    });
+
+                endyear_dropdown.property('value','2020');          
+            
+            });    
+}
+
+       
+init();
+
+  
+
